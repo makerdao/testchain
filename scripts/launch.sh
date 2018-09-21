a7a37b9d96d9b3f499b50acd8e7db551980b97ea
Lawrence Wang <l@lw.io>
2018-09-20 18:29:37 -0700

    setup ganache to use stored chaindata directories
    copy the selected directory into var/chaindata, which is gitignored, so we don't have clean up git changes after the chain is modified.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,28 +1,36 @@
 #!/usr/bin/env bash
 set -e
 
+# parse arguments
+endargs=()
+while [[ "$#" > 0 ]]; do case $1 in
+  -d|--deploy) deploy=1;;
+  -f|--fast) fast=1;;
+  -c|--chaindata) chaindata="$2"; shift;;
+  *) endargs+=($1);;
+esac; shift; done
+
 START_TIME=`date +%s`
 CWD="${0%/*}"
 LIB=$CWD/../lib
-DB=$CWD/../chaindata
+VAR=$CWD/../var
 
-SEED_WORDS="hill law jazz limb penalty escape public dish stand bracket blue jar"
+if [ ! "$deploy" ]; then
+  echo "Using chaindata stored on disk."
+  if [ "$chaindata" ]; then
+    echo "Copying chaindata/$chaindata".
+    rm -rf $VAR/chaindata
+    cp -r $CWD/../chaindata/$chaindata $VAR/chaindata
+  fi
+fi
 
-# If 'fast' is passed as last arg, set the SKIP_BUILD_IF_ALREADY_BUILT and
-# SKIP_SUBMODULE_UPDATE env variables for many speed, much fast, very impress
-if [[ "${@:$#}" == 'fast' ]]; then
-
+if [ $fast ]; then
   export SKIP_BUILD_IF_ALREADY_BUILT=1
   export SKIP_SUBMODULE_UPDATE=1
-  set -- "${@:1:$(($#-1))}" # Pop last arg
 fi
 
 # Check if a testnet is already running on port 2000
 if ! nc -z 127.0.0.1 2000; then
-
-  if [ -n "$SKIP_BUILD_IF_ALREADY_BUILT" ]; then
-    echo "Skipping build of dapps that are already built."
-  fi
 
   if [ -n "$SKIP_SUBMODULE_UPDATE" ]; then
     echo "Skipping git submodule update."
@@ -38,8 +46,10 @@ if ! nc -z 127.0.0.1 2000; then
 
   # Start a testnet on port 2000
   echo "Starting ganache-cli..."
-  ./node_modules/.bin/ganache-cli -i 999 -p 2000 -a 1000 -m $SEED_WORDS \
-    --db $DB > ./ganache.out 2>&1 & netpid=$!
+  ./node_modules/.bin/ganache-cli -i 999 -p 2000 -a 1000 \
+    -m "hill law jazz limb penalty escape public dish stand bracket blue jar" \
+    --db $VAR/chaindata \
+    > $VAR/ganache.out 2>&1 & netpid=$!
 
   # Stop the testnet when this script exits
   trap "kill $netpid" EXIT
@@ -51,7 +61,12 @@ if ! nc -z 127.0.0.1 2000; then
   export ETH_FROM=$(seth rpc eth_coinbase)
 
   # Deploy contracts
-  $CWD/deploy.sh
+  if [ $deploy ]; then
+    if [ "$SKIP_BUILD_IF_ALREADY_BUILT" ]; then
+      echo "Skipping build of dapps that are already built."
+    fi
+    $CWD/deploy.sh
+  fi
 
   END_TIME=`date +%s`
   ELAPSED=`echo $END_TIME - $START_TIME | bc`
